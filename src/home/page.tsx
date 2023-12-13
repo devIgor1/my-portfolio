05a7b08dcb1ed319d702d4a2f5e05c630d85f794
Igor <igormraesmm@gmail.com>
2023-12-13 09:48:00 -0300

    feat: add tech images

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -3,13 +3,26 @@ import { GrProjects } from "react-icons/gr"
 import { FaPhoneAlt } from "react-icons/fa"
 import { SiStarship } from "react-icons/si"
 import { IoPeopleCircle } from "react-icons/io5"
+import nodeIcon from "../assets/node-js.svg"
+import cssIcon from "../assets/css.svg"
+import firebaseIcon from "../assets/firebase.png"
+import gitIcon from "../assets/git.svg"
+import htmlIcon from "../assets/html.svg"
+import javascriptIcon from "../assets/javascript.svg"
+import typescriptIcon from "../assets/typescript.png"
+import reactIcon from "../assets/react.svg"
+import postgreIcon from "../assets/postgresql.svg"
+import prismaIcon from "../assets/prisma.svg"
+import tailwindIcon from "../assets/tailwindcss.svg"
+import mongodbIcon from "../assets/mongodb.svg"
+import githubIcon from "../assets/github.png"
 
 export default function Home() {
   return (
-    <main className="bg-bgPurple min-h-screen min-w-full">
-      <div className="w-full min-h-screen bg-purple-gradient bg-center">
-        <div className="pt-10">
-          <Container>
+    <main className="bg-bgPurple min-h-screen min-w-full w-full bg-purple-gradient bg-center">
+      <Container>
+        <div className="w-full min-h-screen bg-center">
+          <div className="pt-10">
             <div className="relative">
               <div className="absolute blur inset-0 bg-white rounded-full"></div>
               <nav className="relative font-poppins flex flex-row items-center border-t border-[#303034] justify-between h-[53px] px-5 bg-bgPurple rounded-full">
@@ -27,7 +40,7 @@ export default function Home() {
                         <span className="max-[420px]:hidden">
                           <GrProjects color="#fff" />
                         </span>
-                        Projetos
+                        Projects
                       </a>
                     </li>
                     <li className="flex items-center gap-2">
@@ -35,7 +48,7 @@ export default function Home() {
                         <IoPeopleCircle color="#fff" size={25} />
                       </span>
                       <a className="text-sm lg:text-lg text-white  hover:scale-105 cursor-pointer duration-300 flex items-center gap-2">
-                        Redes Sociais
+                        Social Media
                       </a>
                     </li>
                     <li className="flex items-center gap-2">
@@ -43,7 +56,7 @@ export default function Home() {
                         <FaPhoneAlt color="#fff" />
                       </span>
                       <a className="text-sm  lg:text-lg text-white  hover:scale-105 cursor-pointer duration-300 flex items-center gap-2">
-                        Contato
+                        Contact
                       </a>
                     </li>
                   </ul>
@@ -53,23 +66,23 @@ export default function Home() {
             <div className="mt-72 flex items-center justify-center mb-5">
               <h1 className="text-gray text-4xl lg:text-5xl font-nunitoSans font-bold italic flex gap-2 max-[355px]:text-3xl">
                 <SiStarship />
-                Olá eu sou o Igor,
+                Hi, I am Igor,
               </h1>
             </div>
             <div className="flex items-center justify-center flex-col gap-5">
               <h1 className="text-5xl lg:text-6xl font-bold bg-gradient-to-b from-slate-50 to-zinc-300 bg-clip-text text-transparent max-[355px]:text-4xl">
-                Desenvolvedor
-              </h1>
-              <h1 className="text-5xl lg:text-6xl font-bold bg-gradient-to-b from-slate-50 to-zinc-400 bg-clip-text text-transparent max-[355px]:text-4xl">
                 Full-Stack
               </h1>
+              <h1 className="text-5xl lg:text-6xl font-bold bg-gradient-to-b from-slate-50 to-zinc-400 bg-clip-text text-transparent max-[355px]:text-4xl">
+                Developer
+              </h1>
               <a className="font-poppins bg-[#c35dd810] min-w-xl flex items-center hover:text-purewhite  hover:bg-[#c35dd830] text-sm md:text-md 2xl:text-md text-white px-5 py-[8px]  md:px-6 md:py-2 transition-all  ease-in-out duration-300 rounded-full shadow-inner shadow-[#bf97ff70] border border-[#816bac] cursor-pointer">
-                Sobre mim
+                About me
               </a>
             </div>
-          </Container>
+          </div>
         </div>
-      </div>
+      </Container>
     </main>
   )
 }

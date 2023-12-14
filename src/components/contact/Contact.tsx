7799f7077f48f57d25bef32159579a2beb964ebf
Igor <igormraesmm@gmail.com>
2023-12-14 18:11:15 -0300

    feat: add send email functionality

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,25 +1,73 @@
+import { FormEvent, useState } from "react"
 import contact from "../../assets/contact.jpg"
 import button from "../../assets/sendButton.png"
+import emailJs from "@emailjs/browser"
 
 export const Contact = () => {
+  const [email, setEmail] = useState<string>("")
+  const [message, setMessage] = useState<string>("")
+
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (!email || !message) {
+      alert("Please Fill in All Fields")
+      return
+    }
+
+    const templateParams = {
+      message: message,
+      email: email,
+    }
+
+    emailJs
+      .send(
+        "service_7jqjgjp",
+        "template_cypt4ep",
+        templateParams,
+        "BI2LsbjFEQplx7iwi"
+      )
+      .then(() => {
+        alert("Send")
+        setEmail("")
+        setMessage("")
+      })
+  }
+
   return (
     <div id="contact">
       <div className="flex flex-col md:flex-row items-center justify-between mt-96">
         <section className="w-[620px] max-w-full">
-          <form className="flex flex-col w-full font-poppins">
-            <label className="text-3xl bg-gradient-to-b from-slate-50 to-zinc-400 bg-clip-text text-transparent text-center">
+          <form
+            className="flex flex-col w-full font-poppins"
+            onSubmit={sendEmail}
+          >
+            <label className="mb-4 text-3xl bg-gradient-to-b from-slate-50 to-zinc-400 bg-clip-text text-transparent text-center">
               Email
             </label>
-            <input
-              className="mb-10 h-9 p-4 bg-gradient-to-bl from-violet-600 to-bgPurple rounded outline-none font-bold text-white"
-              type="email"
-            />
-            <label className="text-3xl bg-gradient-to-b from-slate-50 to-zinc-400 bg-clip-text text-transparent text-center">
+
+            <div className="relative h-9">
+              <div className="absolute blur inset-0 bg-purple-900 rounded-lg animate-pulse"></div>
+              <input
+                className="relative w-full mb-10 h-9 p-4 bg-bgPurple rounded-lg outline-none font-bold text-white"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <label className="mb-4 text-3xl bg-gradient-to-b from-slate-50 to-zinc-400 bg-clip-text text-transparent text-center mt-14">
               Message
             </label>
-            <textarea className="h-36 p-4 bg-gradient-to-bl from-violet-600 to-bgPurple rounded resize-none outline-none font-bold text-white"></textarea>
+            <div className="relative h-36">
+              <div className="absolute blur inset-0 bg-purple-900 rounded-lg animate-pulse"></div>
+              <textarea
+                className="relative w-full h-36 p-4 bg-bgPurple rounded-lg resize-none outline-none font-bold text-white"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+            </div>
             <div className="flex items-center justify-center w-[120px] mx-auto md:mt-5">
-              <button className="hover:scale-90 duration-300">
+              <button type="submit" className="hover:scale-90 duration-300">
                 <img className="w-full" src={button} alt="send-email-button" />
               </button>
             </div>

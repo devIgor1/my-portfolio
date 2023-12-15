d0d53e97be78911059a6bc5f6ba5037494f9909a
Igor <igormraesmm@gmail.com>
2023-12-14 23:29:38 -0300

    feat: remove unnecessary imports

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast"
 import { Outlet } from "react-router-dom"
 
 export function Layout() {
   return (
     <>
       <Outlet />
+      <Toaster position="top-center" />
     </>
   )
 }

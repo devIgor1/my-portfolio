ffb258838419e9af3d6659b0e423cb28f71a3bf9
devIgor1 <igormraesmm@gmail.com>
2024-03-04 10:15:37 -0300

    style: change navbar font size

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
       <div className="relative">
         <div className="absolute blur inset-0 bg-white rounded-full animate-pulse"></div>
         <nav className="relative flex flex-row items-center border-t border-[#303034] justify-between h-[45px] px-5 bg-bgPurple rounded-full ">
-          <img className="w-[25px] md:w-[60px]" src={logo} alt="" />
+          <img className="w-[55px] md:w-[60px]" src={logo} alt="" />
           <div className="flex gap-2 text-white"></div>
           <div>
             <ul className="flex gap-5 ">
@@ -20,7 +20,7 @@ export const NavBar = () => {
                 <a
                   onClick={() => handleNavClick("projects")}
                   href="#projects"
-                  className="text-sm lg:text-lg  hover:scale-105 cursor-pointer duration-300 flex items-center gap-2 bg-gradient-to-b from-slate-50 to-zinc-300 bg-clip-text text-transparent"
+                  className="text-base lg:text-lg  hover:scale-105 cursor-pointer duration-300 flex items-center gap-2 bg-gradient-to-b from-slate-50 to-zinc-300 bg-clip-text text-transparent"
                 >
                   <span className="max-[420px]:hidden text-zinc-400">
                     <GrProjects />
@@ -35,7 +35,7 @@ export const NavBar = () => {
                 <a
                   onClick={() => handleNavClick("contact")}
                   href="#contact"
-                  className="text-sm  lg:text-lg hover:scale-105 cursor-pointer duration-300 flex items-center gap-2 bg-gradient-to-b from-slate-50 to-zinc-300 bg-clip-text text-transparent"
+                  className="text-base  lg:text-lg hover:scale-105 cursor-pointer duration-300 flex items-center gap-2 bg-gradient-to-b from-slate-50 to-zinc-300 bg-clip-text text-transparent"
                 >
                   Contact
                 </a>
